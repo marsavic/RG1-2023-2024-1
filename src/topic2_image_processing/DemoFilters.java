aebd40e30dfcee208206d3631379221c3f97aa3f
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2023-10-25 17:11:03 +0200

    Update DemoFilters.java

--- src/topic2_image_processing/DemoFilters.java
+++ src/topic2_image_processing/DemoFilters.java
@@ -10,55 +10,67 @@ import mars.drawingx.drawing.View;
 import mars.drawingx.gadgets.annotations.GadgetBoolean;
 import mars.drawingx.gadgets.annotations.GadgetInteger;
 import mars.geometry.Vector;
+import topic2_image_processing.filters.CombinedFilter;
 import topic2_image_processing.filters.ConvolutionFilter;
 import topic2_image_processing.filters.Filter;
+import topic2_image_processing.filters.color.Accent;
+import topic2_image_processing.filters.color.Colorize;
+import topic2_image_processing.filters.color.Desaturate;
 import topic2_image_processing.filters.color.Grayscale;
 import topic2_image_processing.filters.color.Invert;
+import topic2_image_processing.filters.color.Saturate;
+import topic2_image_processing.filters.color.Sepia;
+import topic2_image_processing.filters.displacement.FlipHorizontal;
 import topic2_image_processing.filters.displacement.FlipVertical;
+import topic2_image_processing.filters.displacement.Jitter;
+import topic2_image_processing.filters.displacement.Lens;
+import topic2_image_processing.filters.displacement.Rotate180;
+import topic2_image_processing.filters.displacement.Swirl;
+import topic2_image_processing.filters.displacement.Wave;
 import topic2_image_processing.filters.displacement.Zoom;
 import topic2_image_processing.filters.misc.Sobel;
+import topic2_image_processing.filters.misc.Vignette;
 
 
 public class DemoFilters implements Drawing {
 	@GadgetInteger(min = 0, max = 13)
 	Integer imageIndex = 0;
 	
-	@GadgetInteger(min = 0, max = 8)
+	@GadgetInteger(min = 0, max = 21)
 	Integer filterIndex = 0;
 	
 	@GadgetBoolean
 	Boolean applyFilter = false;
 	
 	
-	Filter[] filters = {			
-			new Grayscale(),
-			new Invert(),
-
-			new FlipVertical(),
-			new Zoom(2.0),
-			
-			new ConvolutionFilter(ConvolutionFilter.BOX_BLUR_3x3),
-			new ConvolutionFilter(ConvolutionFilter.BLUR_5x5),
-			new ConvolutionFilter(ConvolutionFilter.SHARPEN),
-			new ConvolutionFilter(ConvolutionFilter.DETECT_EDGES),
-			new Sobel(),
+	Filter[] filters = {
+			new Sepia(),
+			new Desaturate(0.6),
+			new Saturate(0.5),
+			new Colorize(216, 0.5),
+			new Accent(0),
+			new FlipHorizontal(),
+			new Rotate180(),
+			new Wave(5, 100),
+			new Jitter(10.0),
+			new Vignette(),
 	};
 
 	String[] fileNames = {
-			"monalisa.png",
-			"building.png",
-			"catparty.png",
-			"christmas.png",
-			"couple.png",
-			"dive.png",
-			"doggo.png",
-			"fall.png",
-			"forecast.png",
-			"kitchen.png",
-			"meterologist.png",
-			"office.png",
-			"skirts.png",
-			"waiting.png",
+			"monalisa.jpg",
+			"building.jpg",
+			"catparty.jpg",
+			"christmas.jpg",
+			"couple.jpg",
+			"dive.jpg",
+			"doggo.jpg",
+			"fall.jpg",
+			"forecast.jpg",
+			"kitchen.jpg",
+			"meterologist.jpg",
+			"office.jpg",
+			"skirts.jpg",
+			"waiting.jpg",
 	};
 	
 
