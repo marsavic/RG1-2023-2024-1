59328d57d4b1dae03f41dc8e03566f44ef79d327
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2023-10-26 11:54:23 +0200

    Update Swirl.java

--- src/topic2_image_processing/filters/displacement/Swirl.java
+++ src/topic2_image_processing/filters/displacement/Swirl.java
@@ -16,7 +16,11 @@ public class Swirl extends DisplacementFilter {
 
 	@Override
 	public Vector source(Vector dst, Vector dim) {
-		return null;
+		Vector s = dst.div(dim).mul(2).sub(new Vector(1, 1)); // Pretvaramo poziciju u normalizovane koordinate. Vektor s se nalazi u [-1,1]x[-1,1]. 
+		double r = s.norm();                                  // Udaljenost od centra, vrednost iz [0,1].
+		double phi = s.angle() + a * Math.cos(f*r*Math.PI*2); // s.angle() je ugao koji vektor s zaklapa sa x-osom. Na to dodajemo pomeraj prema navedenoj funkciji.
+		Vector p = Vector.polar(r, phi);                      // Trazenu poziciju dobijamo sa izracunatim novim uglom, a istom udaljenosti od centra.	
+		return p.add(new Vector(1, 1)).div(2).mul(dim);       // Vracamo se u koordinatni sistem slike.
 	}
 	
 }
