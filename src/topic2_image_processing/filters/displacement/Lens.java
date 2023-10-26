472e6c71fb62416dc8787869cc4ec76775d4b979
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2023-10-26 11:53:54 +0200

    Update Lens.java

--- src/topic2_image_processing/filters/displacement/Lens.java
+++ src/topic2_image_processing/filters/displacement/Lens.java
@@ -15,7 +15,12 @@ public class Lens extends DisplacementFilter {
 
 	@Override
 	public Vector source(Vector dst, Vector dim) {
-		return null;
+		Vector s = dst.div(dim).mul(2).sub(new Vector(1, 1));
+		double r = s.norm();                                  // Udaljenost od centra, normalizovana tako da je radius 1.
+		double phi = s.angle();                               // Ugao koji zaklapa sa x-osom
+		double l = Math.pow(r, f);                            // Udaljenost src-a od centra je funkcija udaljenosti dst-a od centra		
+		Vector p = Vector.polar(l, phi);                      // Trazena pozicija izrazena u normalizovanim koordinatama	
+		return p.add(new Vector(1, 1)).div(2).mul(dim);       // Vracamo se u koordinatni sistem slike 
 	}
 	
 }
