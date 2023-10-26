be95fbf1d9caac9db0e261707970a3948ad471b7
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2023-10-26 11:52:29 +0200

    Update Saturate.java

--- src/topic2_image_processing/filters/color/Saturate.java
+++ src/topic2_image_processing/filters/color/Saturate.java
@@ -19,7 +19,12 @@ public class Saturate extends ColorFilter {
 	public Color processColor(Color input) {
 		double s = input.getSaturation();
 		
-		return null;
+		return Color.hsb(
+				input.getHue(),
+				s + k * (1 - s),          // Linearna interpolacija. Kada k ide od 0 do 1, ovaj izraz ide od s do 1.
+				input.getBrightness(),
+				input.getOpacity()
+				);
 	}
 	
 }	
