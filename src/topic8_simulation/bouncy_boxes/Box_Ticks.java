f901d3380b065f2b847397cd7fc79aeae139a6df
gitLMgit <115313459+gitLMgit@users.noreply.github.com>
2023-12-27 19:22:13 +0100

    Add files via upload

--- src/topic8_simulation/bouncy_boxes/Box_Ticks.java
+++ src/topic8_simulation/bouncy_boxes/Box_Ticks.java
@@ -14,14 +14,45 @@ public class Box_Ticks extends Box {
 	Vector areaD;
 
 	
+	
+	public Box_Ticks(Vector areaD, Vector p, Vector v, Color color) {
+		this.areaD = areaD;
+		this.p = p;
+		this.v = v;
+		this.color = color;
+		
+		this.t = 0;
+	}		
+
+	
+	private void update() {
+		if (p.x < 0      ) v = new Vector(-v.x,  v.y);
+		if (p.x > areaD.x) v = new Vector(-v.x,  v.y);
+		if (p.y < 0      ) v = new Vector( v.x, -v.y);
+		if (p.y > areaD.y) v = new Vector( v.x, -v.y);
+		
+		p = p.add(v.mul(intervalTick));
+		t += intervalTick;
+	}
+
+	
+	private void updateTo(double time) {
+		while (t + intervalTick <= time) {
+			update();
+		}
+	}
+	
+	
 	@Override
 	public Vector getPosition(double time) {
+		updateTo(time);
 		return p;
 	}
 	
 	
 	@Override
 	public Vector getVelocity(double time) {
+		updateTo(time);
 		return v;
 	}
 
